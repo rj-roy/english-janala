93ac8c10f4f57eb37954644b346b9d44c767ee23
rj-roy <dpjdeveloper.me@gmail.com>
2026-03-05 00:40:38 +0600

    Vocabularies Added by api-integration

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -32,20 +32,32 @@ const displayData = (level) => {
                 .then((data) => addDesc(data.data))
 
             const addDesc = (lessonByLevel) => {
-                const lessonDesk = document.querySelector("lesson-desc");
+                const lessonDesk = document.querySelector("#lesson-desc");
                 lessonDesk.innerHTML = "";
-                
-                lessonByLevel.forEach(dtDesc => {                    
+
+                if(!lessonByLevel || lessonByLevel.length === 0){
+                    console.log("null")
+                }
+
+                lessonByLevel.forEach(dtDesc => {
                     const card = document.createElement('div');
-                    card.classList.add('bg-white', 'space-y-3', 'p-6')
+                    card.classList.add('grid', 'w-full', 'bg-white', 'space-y-5', 'p-6', 'shadow-sm', 'rounded-sm')
                     card.innerHTML = `
-                    <p class = "">${dtDesc.word}</p>
-                    <p class = "">Meaning/Pronunciation</p>
-                    <p class = "">${dtDesc.meaning} / ${dtDesc.pronunciation}</p>
+                    <div class="grid space-y-2">
+                        <p class="font-bold">${dtDesc.word}</p>
+                        <p class="">Meaning/Pronunciation</p>
+                        <p class="font-bold font-bng">${dtDesc.meaning} / ${dtDesc.pronunciation}</p>
+                    </div>
+                
+                    <div id="buttons-temp" class="flex justify-between items-center">
+                        <button class="btn hover:bg-blue-500"><i class="fa-solid fa-circle-info "></i></button>
+
+                        <button class="btn hover:bg-blue-500"><i class="fa-solid fa-volume-high "></i></button>
+                    </div>
                     `
 
                     lessonDesk.appendChild(card);
-                })
+                });
             }
         })
     });
